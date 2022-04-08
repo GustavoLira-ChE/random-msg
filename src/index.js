bffa0788e23a3ae5de2d964d194aeb2ee665edd8
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-08 12:42:23 -0500

    add: new messages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const messages = ["Tú puedes", "Sigue programando", "La constancia es la clave del éxito", "La disciplina te hará mejorar día con día"];
+const messages = ["Tú puedes", "Sigue programando", "La constancia es la clave del éxito", "La disciplina te hará mejorar día con día","Sigue intentandole","la meditación es necesaria para recuperar la mente"];
 
 const randomMsg = () =>{
     const message = messages[Math.floor(Math.random() * messages.length)];
